1d4096471f0b57673d3881e1dfe40e2652d86104
MarkB3 <mpb220@exeter.ac.uk>
2017-03-22 21:44:53 +0000

    Reworked a lot of stuff

--- src/echo/Computational/StartListeningListener.java
+++
@@ -1,9 +0,0 @@
-package echo.Computational;
-
-/**
- * @version 1.0
- * Listener that is informed when the Echo device is ready to listen
- */
-public interface StartListeningListener {
-    void startListening();
-}
